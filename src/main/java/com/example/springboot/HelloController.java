28eb25a8ae75ff6e4faefb129a87ccc72bdbd403
HugoXiao1984 <54834326+HugoXiao1984@users.noreply.github.com>
2022-08-02 22:11:35 +0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + iteration 020!";
+		return "Greetings from Spring Boot + Tanzu + iteration 021!";
 	}
 
 }
